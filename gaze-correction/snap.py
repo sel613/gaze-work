8f5edc1672cb84e6865bb2a778c2c070cf937da5
sel613 <ramasamyselvapriyanka@gmail.com>
2022-09-13 18:20:10 +0530

    updated

--- gaze-correction/snap.py
+++ gaze-correction/snap.py
@@ -21,7 +21,6 @@ def detectFacialLandmarks(image, face_mesh, display = True):
     This function performs facial landmarks detection on an image.
     
     '''
-    
     # Perform the facial landmarks detection on the image, after converting it into RGB format.
     results = face_mesh.process(image[:,:,::-1])
     
@@ -49,73 +48,33 @@ def detectFacialLandmarks(image, face_mesh, display = True):
                                       connection_drawing_spec=mp_drawing_styles.get_default_face_mesh_contours_style())
  
     # Check if the original input image and the output image are specified to be displayed.
-    if display:
-        
-        # Display the original input image and the output image.
-        plt.figure(figsize=[15,15])
-        plt.subplot(121);plt.imshow(image[:,:,::-1]);plt.title("Original Image");plt.axis('off');
-        plt.subplot(122);plt.imshow(output_image);plt.title("Output");plt.axis('off');
-        
-    # Otherwise
-    else:
-        
+    if not display:
         # Return the output image in BGR format and results of facial landmarks detection.
         return np.ascontiguousarray(output_image[:,:,::-1], dtype=np.uint8), results   
+
 def isOpen(image, face_mesh_results, face_part, threshold=5, display=True):
     '''
     This function checks whether the an eye or mouth of the person(s) is open, 
     utilizing its facial landmarks.
     '''
-    
-    # Retrieve the height and width of the image.
     image_height, image_width, _ = image.shape
     
-    # Create a copy of the input image to write the isOpen status.
-    output_image = image.copy()
-    
+   
     # Create a dictionary to store the isOpen status of the face part of all the detected faces.
     status={}
-    
-    # Check if the face part is mouth.
-    if face_part == 'MOUTH':
-        
-        # Get the indexes of the mouth.
-        INDEXES = mp_face_mesh.FACEMESH_LIPS
-        
-        # Specify the location to write the is mouth open status.
-        loc = (10, image_height - image_height//40)
-        
-        # Initialize a increment that will be added to the status writing location, 
-        # so that the statuses of two faces donot overlap. 
-        increment=-30
-        
-    # Check if the face part is left eye.    
-    elif face_part == 'LEFT EYE':
-        
-        # Get the indexes of the left eye.
-        INDEXES = mp_face_mesh.FACEMESH_LEFT_EYE
-        
-        # Specify the location to write the is left eye open status.
-        loc = (10, 30)
-        
-        # Initialize a increment that will be added to the status writing location, 
-        # so that the statuses of two faces donot overlap.
-        increment=30
-    
     # Check if the face part is right eye.    
-    elif face_part == 'RIGHT EYE':
-        
-        # Get the indexes of the right eye.
+    if face_part == 'RIGHT EYE':
         INDEXES = mp_face_mesh.FACEMESH_RIGHT_EYE 
+    elif face_part == 'LEFT EYE' :
+        INDEXES = mp_face_mesh.FACEMESH_LEFT_EYE   
+    #     # # Specify the location to write the is right eye open status.
+    #     # loc = (image_width-300, 30)
         
-        # Specify the location to write the is right eye open status.
-        loc = (image_width-300, 30)
-        
-        # Initialize a increment that will be added to the status writing location, 
-        # so that the statuses of two faces donot overlap.
-        increment=30
+    #     # Initialize a increment that will be added to the status writing location, 
+    #     # so that the statuses of two faces donot overlap.
+    #     # increment=30
     
-    # Otherwise return nothing.
+    # # Otherwise return nothing.
     else:
         return
     
@@ -141,102 +100,45 @@ def isOpen(image, face_mesh_results, face_part, threshold=5, display=True):
         else:
             # Set status of the face part to close.
             status[face_no] = 'CLOSE'
-            
-            # Set color which will be used to write the status to red.
-            color=(0,0,255)
-        
-        # Write the face part isOpen status on the output image at the appropriate location.
-        cv2.putText(output_image, f'FACE {face_no+1} {face_part} {status[face_no]}.', 
-                    (loc[0],loc[1]+(face_no*increment)), cv2.FONT_HERSHEY_PLAIN, 1.4, color, 2)
-                
-    # Check if the output image is specified to be displayed.
-    if display:
- 
-        # Display the output image.
-        plt.figure(figsize=[10,10])
-        plt.imshow(output_image[:,:,::-1]);plt.title("Output Image");plt.axis('off');
-    
-    # Otherwise
-    else:
-        
         # Return the output image and the isOpen statuses of the face part of each detected face.
-        return output_image, status
+        return status
+
 def getSize(image, face_landmarks, INDEXES):
     '''
     This function calculate the height and width of a face part utilizing its landmarks.
     '''
-    
-    # Retrieve the height and width of the image.
     image_height, image_width, _ = image.shape
     
     # Convert the indexes of the landmarks of the face part into a list.
     INDEXES_LIST = list(itertools.chain(*INDEXES))
-    
-    # Initialize a list to store the landmarks of the face part.
     landmarks = []
-    
-    # Iterate over the indexes of the landmarks of the face part. 
     for INDEX in INDEXES_LIST:
-        
-        # Append the landmark into the list.
         landmarks.append([int(face_landmarks.landmark[INDEX].x * image_width),
                                int(face_landmarks.landmark[INDEX].y * image_height)])
-    
-    # Calculate the width and height of the face part.
     _, _, width, height = cv2.boundingRect(np.array(landmarks))
-    
-    # Convert the list of landmarks of the face part into a numpy array.
     landmarks = np.array(landmarks)
-    
-    # Retrurn the calculated width height and the landmarks of the face part.
     return width, height, landmarks
+
 def overlay(image, filter_img, face_landmarks, face_part, INDEXES, display=True):
     '''
     This function will overlay a filter image over a face part of a person in the image/frame.
     '''
-    
-    # Create a copy of the image to overlay filter image on.
     annotated_image = image.copy()
-    
-    # Errors can come when it resizes the filter image to a too small or a too large size .
-    # So use a try block to avoid application crashing.
     try:
-    
-        # Get the width and height of filter image.
         filter_img_height, filter_img_width, _  = filter_img.shape
- 
-        # Get the height of the face part on which we will overlay the filter image.
         _, face_part_height, landmarks = getSize(image, face_landmarks, INDEXES)
-        
-        # Specify the height to which the filter image is required to be resized.
         required_height = int(face_part_height*2.5)
-        
-        # Resize the filter image to the required height, while keeping the aspect ratio constant. 
         resized_filter_img = cv2.resize(filter_img, (int(filter_img_width*
                                                          (required_height/filter_img_height)),
                                                      required_height))
         
-        # Get the new width and height of filter image.
         filter_img_height, filter_img_width, _  = resized_filter_img.shape
- 
-        # Convert the image to grayscale and apply the threshold to get the mask image.
         _, filter_img_mask = cv2.threshold(cv2.cvtColor(resized_filter_img, cv2.COLOR_BGR2GRAY),
                                            25, 255, cv2.THRESH_BINARY_INV)
  
-        # Calculate the center of the face part.
         center = landmarks.mean(axis=0).astype("int")
- 
-        # Check if the face part is mouth.
-        if face_part == 'MOUTH':
- 
-            # Calculate the location where the smoke filter will be placed.  
-            location = (int(center[0] - filter_img_width / 3), int(center[1]))
- 
-        # Otherwise if the face part is an eye.
-        else:
- 
-            # Calculate the location where the eye filter image will be placed.  
-            location = (int(center[0]-filter_img_width/2), int(center[1]-filter_img_height/2))
+        # Calculate the location where the eye filter image will be placed.  
+        location = (int(center[0]-filter_img_width/2), int(center[1]-filter_img_height/2))
  
         # Retrieve the region of interest from the image where the filter image will be placed.
         ROI = image[location[1]: location[1] + filter_img_height,
@@ -269,9 +171,6 @@ def overlay(image, filter_img, face_landmarks, face_part, INDEXES, display=True)
 
 camera_video = cv2.VideoCapture(0)
  
-# Create named window for resizing purposes.
-cv2.namedWindow('Replaced EyeMotion', cv2.WINDOW_NORMAL)
-
 lefteye_animation = cv2.VideoCapture('gaze-correction/ezgif-4-1d0457a163.mp4')
 lefteye_frame_counter = 0
 # Iterate until the webcam is accessed successfully.
@@ -298,35 +197,28 @@ while camera_video.isOpened():
     if face_mesh_results.multi_face_landmarks:
         
         # Get the left eye isOpen status of the person in the frame.
-        _, left_eye_status = isOpen(frame, face_mesh_results, 'LEFT EYE', 
+        left_eye_status = isOpen(frame, face_mesh_results, 'LEFT EYE', 
                                         threshold=4.5 , display=False)
         
         # Get the right eye isOpen status of the person in the frame.
-        _, right_eye_status = isOpen(frame, face_mesh_results, 'RIGHT EYE', 
+        right_eye_status = isOpen(frame, face_mesh_results, 'RIGHT EYE', 
                                          threshold=4.5, display=False)
         
         # Iterate over the found faces.
         for face_num, face_landmarks in enumerate(face_mesh_results.multi_face_landmarks):
             if right_eye_status[face_num] == 'OPEN':
-                
                 # Overlay the right eye image on the frame at the appropriate location.
                 frame = overlay(frame, lefteye_frame, face_landmarks,
                                 'RIGHT EYE', mp_face_mesh.FACEMESH_RIGHT_EYE, display=False)
             
             if left_eye_status[face_num] == 'OPEN':
-                
-                # Overlay the smoke animation on the frame at the appropriate location.
                 frame = overlay(frame, lefteye_frame, face_landmarks, 
                                 'LEFT_EYE', mp_face_mesh.FACEMESH_LEFT_EYE, display=False)
 
     # Display the frame.
     cv2.imshow('Face Filter', frame)
-    
-    # Wait for 1ms. If a key is pressed, retreive the ASCII code of the key.
     if cv2.waitKey(10) == 27:
         break
-
- 
-# Release the VideoCapture Object and close the windows.                  
+             
 camera_video.release()
 cv2.destroyAllWindows()
