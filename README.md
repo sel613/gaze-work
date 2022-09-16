77287cd52a984b7cfc7930b6286413498c28420e
sel613 <ramasamyselvapriyanka@gmail.com>
2022-09-16 17:32:50 +0530

    hist without cv

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # gaze-work
+<!-- 
+sobel edge detector
+https://homepages.inf.ed.ac.uk/rbf/HIPR2/sobel.htm
+see opencv implementation and implement it with python (without using opencv) compare the results
+calculate  histogram of the gray image
+see opencv implementation and implement it with python (without using opencv) compare the results
+read about kl divergence and compare kl with CE loss
+read about negative log likelihood  and understand its relation with CE loss -->
--- try-out/histogram.py
+++ try-out/histogram.py
@@ -16,7 +16,43 @@ for i,col in enumerate(color):
     plt.xlim([0,256])
 plt.show()
 
+# without opencv
 
+# function to obtain histogram of an image
+def hist_plot(img):
+	count =[]
+	r = []
+	for k in range(0, 256):
+		r.append(k)
+		count1 = 0
+		for i in range(m):
+			for j in range(n):
+				if img[i, j]== k:
+					count1+= 1
+		count.append(count1)
+	return (r, count)
+img = cv.imread('try-out/cat.jpeg',0)
+m, n = img.shape[:2]
+r1, count1 = hist_plot(img)
+plt.stem(r1, count1)
+plt.xlabel('intensity value(0-255)')
+plt.ylabel('number of pixels')
+plt.title('Histogram of the gray image')
+
+# Transformation to obtain stretching
+constant = (255-0)/(img.max()-img.min())
+img_stretch = img * constant
+r, count = hist_plot(img_stretch)
+
+# plotting the histogram
+plt.stem(r, count)
+plt.xlabel('intensity value')
+plt.ylabel('number of pixels')
+plt.title('Histogram of the stretched image')
+
+# Storing stretched Image
+# cv.imwrite('Stretched Image 4.png', img_stretch)
+plt.show()
 #using opencv
 # create a mask
 mask = np.zeros(img.shape[:2], np.uint8)
